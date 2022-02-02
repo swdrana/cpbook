6a8106939ffa92abf21688b901165b7bab5edbae
swdrana <tctr2s@gmail.com>
2022-02-02 16:13:27 +0600

    VS Code comment shortcurt added

--- cpbook-part-1/ch-02/2.16-how-to-add-comment.c
+++ cpbook-part-1/ch-02/2.16-how-to-add-comment.c
@@ -6,6 +6,14 @@ int main()
     /* We have printed Hello,
     now we shall print World.
     Note that this is a multi-line comment */
+
+
+    // VS Code Single line comment: Mac: Command+/  | Windows: Ctrl+/
+    
+    /* Multiline Comment:
+    Mac: Option+Shift+A
+    Windows: Alt+Shift+A */
+
     printf("World"); // printed world
     return 0;
 }
